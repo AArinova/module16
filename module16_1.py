e4f582ac666bb4a0aae72cd7fc533e454bc12e43
AArinova <aliya-arinova@yandex.ru>
2024-12-14 20:17:17 +0700

    Add files via upload

--- module16_1.py
+++ module16_1.py
@@ -1,1 +1,22 @@
-import fastapi
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Кажется, это главная страница."}
+
+@app.get("/user/admin")
+async def admin():
+    return {"message": "Вы вошли как администратор."}
+
+@app.get("/user/{user_id}")
+async def check_user(user_id: int):
+    return {f"Вы вошли как пользователь № {user_id}."}
+
+@app.get("/user")
+async def get_user_info(uname: str, age: int):
+    return {f"Информация о пользователе. Имя: {uname}, Возраст: {age}."}
+
+
