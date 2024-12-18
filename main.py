13184c8f510963a7549588e02e6bcb2c6c4656be
AArinova <aliya-arinova@yandex.ru>
2024-12-18 18:46:52 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from http.client import HTTPException
 from fastapi import FastAPI, Path
 from typing import Annotated
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
 
 app = FastAPI()
@@ -11,24 +11,32 @@ class User(BaseModel):
     username: str
     age: int
 
+class UserCreate(BaseModel):
+    username: str = Field(..., min_length=5, max_length=100)
+    age: int = Field(..., min=18, max=120)
+
+
 users: List[User]=[User(id =1, username='Example', age =18)]
 
 @app.get("/user", response_model=List[User])
 async def get_users():
     return users
 
-@app.post("/user/{username}/{age}")
-async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, regex="^[a-zA-Z0-9_-]+$", description="Enter username")],
-                   age: Annotated[int, Path(ge=18, le=120, description="Enter age")]
-                   ):
-    new_user_id = int(max(users, key=users.get))+1
-    users[new_user_id] = f"Имя: {username}, возраст: {str(age)}"
-    return f"User {new_user_id} is registered."
+@app.post("/user/{username}/{age}", response_model=User)
+async def create_task(user: UserCreate):
+    new_id = max((t.id for t in users), default=0) + 1
+    new_user = User(id=new_id, username=user.username, age=user.age)
+    users.append(new_user)
+    return new_user
 
-Добавляет в список users объект User.
-id этого объекта будет на 1 больше, чем у последнего в списке users. Если список users пустой, то 1.
-Все остальные параметры объекта User - переданные в функцию username и age соответственно.
-В конце возвращает созданного пользователя.
+    # async def add_user(username: Annotated[
+    #     str, Path(min_length=5, max_length=20, regex="^[a-zA-Z0-9_-]+$", description="Enter username")],
+    #                    age: Annotated[int, Path(ge=18, le=120, description="Enter age")]
+    #                    ):
+    # new_user_id = int(max(users, key=users.get))+1
+    # users[new_user_id] = f"Имя: {username}, возраст: {str(age)}"
+    # return f"User {new_user_id} is registered."
+
 
 @app.put("/user/{user_id}/{username}/{age}")
 async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, regex="^[a-zA-Z0-9_-]+$", description="Enter username")],
